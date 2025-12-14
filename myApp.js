7599041c2ec1a51b6a2e054a01553995726d0de0
mbsj145 <mbsj145@gmail.com>
2025-12-14 22:18:54 +0500

    Establish MongoDB connection with Mongoose
    Add MongoDB connection using Mongoose

--- myApp.js
+++ myApp.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 
 let Person;
